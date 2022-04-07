555e4a98b1a892db9bd24506c2a3ba08837bb8c1
fastmed <maks.memelige@gmail.com>
2022-04-07 16:19:04 +0200

    Add model `City`

--- backend/api/models.py
+++ backend/api/models.py
@@ -5,3 +5,6 @@ from django.db import models
 class Сourier(models.Model):
     id = models.CharField(verbose_name="ID курьера", max_length=255, primary_key=True)
 
+
+class City(models.Model):
+    title = models.CharField(verbose_name="Название города", max_length=60)
