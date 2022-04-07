390f83f1be6aee04bc47570086443458cccfd362
fastmed <maks.memelige@gmail.com>
2022-04-07 16:21:58 +0200

    Add `ProductSerializer`

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,9 @@
 from rest_framework import serializers
 
 from .models import Product, UserCart, City, District, Street, House
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
