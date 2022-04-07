c25eb62153329094485e89b34623cb210488f095
fastmed <maks.memelige@gmail.com>
2022-04-07 16:24:59 +0200

    Add `CityViewSet`

--- backend/api/views.py
+++ backend/api/views.py
@@ -16,3 +16,18 @@ class ProductViewSet(viewsets.GenericViewSet):
     def list(self, request):
         serializer = self.serializer_class(self.queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CityViewSet(viewsets.GenericViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer 
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+   
+    @action(detail=True, methods=["get"], url_name="districts", url_path="districts", serializer_class=DistrictSerializer, queryset=District.objects.all())
+    def districts(self, request, pk=None):
+        districts = self.queryset.filter(city_id=pk)
+        serializer = self.serializer_class(districts, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
