1dba244386cb1a662f970f31060980207d951018
fastmed <maks.memelige@gmail.com>
2022-04-07 16:20:00 +0200

    Add model `House`

--- backend/api/models.py
+++ backend/api/models.py
@@ -19,3 +19,8 @@ class District(models.Model):
 class Street(models.Model):
     title = models.CharField(verbose_name="Название улицы", max_length=60)
     district = models.ForeignKey(City, verbose_name="Район", on_delete=models.CASCADE, related_name="streets")
+
+
+class House(models.Model):
+    title = models.CharField(verbose_name="Название дома", max_length=60)
+    street = models.ForeignKey(City, verbose_name="Улица", on_delete=models.CASCADE, related_name="houses")
