7db4986f398f037faee6342df383e01dc220dc4d
fastmed <maks.memelige@gmail.com>
2022-04-07 16:24:36 +0200

    Add imports for views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+from .models import Product, UserCart, City, District, Street, House
+
+from .serializers import ProductSerializer, UserCartSelizer, CitySerializer, DistrictSerializer, StreetSerializer, HouseSerializer
+
 
 # Create your views here.
