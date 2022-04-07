685777c0991b05f10734049879082a4d5bec648a
fastmed <maks.memelige@gmail.com>
2022-04-07 16:27:07 +0200

    Add sub urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,3 +2,14 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from .views import ProductViewSet, CityViewSet, DistrictViewSet, StreetViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register(r"products", ProductViewSet, basename="products")
+router.register(r"cities", CityViewSet, basename="cities")
+router.register(r"districts", DistrictViewSet, basename="districts")
+router.register(r"streets", StreetViewSet, basename="streets")
+
+urlpatterns = [
+] + router.urls
