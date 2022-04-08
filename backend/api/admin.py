ba8e692c13360059f5f9b9a43f844633a582d7d2
fastmed <maks.memelige@gmail.com>
2022-04-08 19:07:43 +0200

    Add prototype for admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Product, UserCart, City, District, Street, House, Сourier
+
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(UserCart)
+admin.site.register(City)
+admin.site.register(District)
+admin.site.register(Street)
+admin.site.register(House)
+admin.site.register(Сourier)
