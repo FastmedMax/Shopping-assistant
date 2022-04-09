2311c9939aded4744c6193aca7f3febb7bd1517b
fastmed <maks.memelige@gmail.com>
2022-04-09 18:58:10 +0200

    Update imports for views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProductViewSet, CityViewSet, DistrictViewSet, StreetViewSet
+from .views import (
+    UserViewSet,
+    ProductViewSet,
+    CityViewSet,
+    DistrictViewSet,
+    StreetViewSet,
+    UserCartViewSet
+)
 
 app_name = 'api'
 
