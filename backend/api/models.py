6e2d1b8d3b4bdf9a05ead76c318f3429d9a94a4b
fastmed <maks.memelige@gmail.com>
2022-04-09 18:47:57 +0200

    Update `UserCart` model

--- backend/api/models.py
+++ backend/api/models.py
@@ -32,8 +32,8 @@ class User(models.Model):
 
 class UserCart(models.Model):
     user = models.ForeignKey(User, verbose_name="Пользователь", on_delete=models.CASCADE, related_name="carts")
-    price = models.PositiveIntegerField(verbose_name="Итог")
-    address = models.TextField(verbose_name="Адресс")
+    price = models.PositiveIntegerField(verbose_name="Итог", default=0)
+    address = models.TextField(verbose_name="Адресс", blank=True)
 
 
 class Product(models.Model):
