c97b2716db727ae28ea0c67a8601cd1c7dcc7408
fastmed <maks.memelige@gmail.com>
2022-04-10 13:06:57 +0200

    Add `paginator` def

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -28,5 +28,30 @@ class Buy(StatesGroup):
     payment = State()
 
 
+def paginator(objects, name) -> types.InlineKeyboardMarkup:
+    markup = types.InlineKeyboardMarkup()
+
+    num_pages = len(objects) // 8
+    if len(objects) % 8:
+        num_pages += 1
+    if num_pages == 0:
+        num_pages = 1
+
+    for item_1, item_2 in zip_longest(objects[0:8:2], objects[1:8:2]):
+        buttons = []
+        if item_1:
+            buttons.append(types.InlineKeyboardButton(text=item_1['title'], callback_data=f"{name}:{item_1['id']}"))
+        if item_2:
+            buttons.append(types.InlineKeyboardButton(text=item_2['title'], callback_data=f"{name}:{item_2['id']}"))
+        markup.row(*buttons)
+
+    markup.row(
+        types.InlineKeyboardButton("Назад", callback_data=f"previous_{name}"),
+        types.InlineKeyboardButton(f"1/{num_pages}", callback_data="page_count"),
+        types.InlineKeyboardButton("Далее", callback_data=f"next_{name}"),
+    )
+
+    return markup
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
