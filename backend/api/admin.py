59d5b903a5a7bf1dfb754f62f3b8058a7cdac000
fastmed <maks.memelige@gmail.com>
2022-04-10 13:05:09 +0200

    Update admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -7,9 +7,42 @@ from .models import Product, UserCart, City, District, Street, House, Сourier, 
 
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(UserCart)
-admin.site.register(City)
-admin.site.register(District)
-admin.site.register(Street)
-admin.site.register(House)
 admin.site.register(Сourier)
+
+
+class ProductsInline(nested_admin.NestedStackedInline):
+    model = UserProduct
+    extra = 0
+    classes = ["collapse"]
+
+class CartInline(nested_admin.NestedStackedInline):
+    model = UserCart
+    extra = 0
+    inlines = (ProductsInline,)
+    classes = ["collapse"]
+
+@admin.register(User)
+class User(nested_admin.NestedModelAdmin):
+    inlines = (CartInline,)
+
+
+class HouseInline(nested_admin.NestedStackedInline):
+    model = House
+    extra = 0
+    classes = ["collapse"]
+
+class StreetInline(nested_admin.NestedStackedInline):
+    model = Street
+    extra = 0
+    classes = ["collapse"]
+    inlines = (HouseInline,)
+
+class DistrictInline(nested_admin.NestedStackedInline):
+    model = District
+    extra = 0
+    classes = ["collapse"]
+    inlines = (StreetInline,)
+
+@admin.register(City)
+class User(nested_admin.NestedModelAdmin):
+    inlines = (DistrictInline,)
